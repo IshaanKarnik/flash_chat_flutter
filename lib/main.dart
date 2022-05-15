61956b60ab3a380939ec7f47880ac2ee863c280a
Ishaan Karnik <ishaankarnik88@gmail.com>
2022-05-15 20:05:53 +0530

    Firebase Added and Configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat_flutter/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
